38375db62b1fb1e3d6557cb6c0d6ffc41187e7b6
Mykhailo Lapchynskyi <mykhailolapchynskyi@Dikaas-MacBook-Pro.local>
2024-09-30 11:43:55 +0200

    make build

---
+++ index.eb041a99.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("largeImg");document.querySelectorAll("a").forEach(function(t){t.addEventListener("click",function(n){n.preventDefault(),e.src=t.href})});
+//# sourceMappingURL=index.eb041a99.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,12 @@
+"use strict";
+const largeImage = document.getElementById("largeImg");
+const links = document.querySelectorAll("a");
+links.forEach((link)=>{
+    // eslint-disable-next-line no-shadow
+    link.addEventListener("click", (event)=>{
+        event.preventDefault();
+        largeImage.src = link.href;
+    });
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
